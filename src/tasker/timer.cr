e402af01936f67818c458e78a8decee3d5f613b7
Stephen von Takach <steve@vontaka.ch>
2021-05-25 16:08:28 +1000

    feat: refactor timer class to use select statements

--- src/tasker/timer.cr
+++ src/tasker/timer.cr
@@ -1,25 +1,26 @@
 # a class designed to allow sleeping fibers to be cancelled
 class Timer
-  def initialize(sleep_for, &block)
+  def initialize(@sleep_for : Float64, &@callback : -> _)
     @cancelled = false
-    @fiber = Fiber.new do
-      sleep sleep_for
-      block.call unless @cancelled
-    end
+    @cancel = Channel(Bool).new
   end
 
   def start_timer : Nil
-    Fiber.current.enqueue
-    @fiber.resume
+    spawn do
+      select
+      when @cancel.receive
+      when timeout(@sleep_for.seconds)
+        @cancelled = true
+        @callback.call
+      end
+    end
+    Fiber.yield
   end
 
   def cancel : Bool
     return true if @cancelled
     @cancelled = true
-    if !@fiber.dead? && @fiber.resumable?
-      Fiber.current.enqueue
-      @fiber.resume
-    end
-    @cancelled
+    @cancel.send(true)
+    true
   end
 end
